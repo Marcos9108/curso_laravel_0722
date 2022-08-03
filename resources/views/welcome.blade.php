9c5db9ab5ceed092f6887ed06c5e6c0397bcbf35
Hazibe <cjoselyn771@gmail.com>
2022-08-03 09:42:26 -0500

    Se modifico archivo - resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,8 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    
+                    <a href="http://localhost/curso_laravel_0722/public/login"> BTN Haz </a>
                 </div>
             </div>
         </div>
