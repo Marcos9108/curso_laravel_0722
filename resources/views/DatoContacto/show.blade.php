0ecfc0a55c50e5c143dbe1ad50b817aad743ca4d
Hazibe <cjoselyn771@gmail.com>
2022-08-03 23:19:02 -0500

    EDIT FILE - resouces/views/DatoContacto/show.blade.php

--- resources/views/DatoContacto/show.blade.php
+++ resources/views/DatoContacto/show.blade.php
@@ -6,7 +6,7 @@
 
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Datos de empleado</h3>
+                        <h3 class="panel-title">Datos de Contacto empleado</h3>
                     </div>
                     <div class="panel-body">
                         <div class="table-container">
@@ -14,31 +14,31 @@
                             <div class="row">
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label class="form-control">Nombre: {{$empleado->nombre}}</label>
+                                        <label class="form-control">Nombre: {{$datoContacto->nombre}}</label>
                                     </div>
                                 </div>
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label class="form-control">Puesto: {{$empleado->puesto}}</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <div class="form-group">
-                                        <label class="form-control">Edad: {{$empleado->edad}}</label>
-                                    </div>
-                                </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <div class="form-group">
-                                        <label class="form-control">Salario: {{$empleado->salario}}</label>
+                                        <label class="form-control">Email: {{$datoContacto->email}}</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label class="form-control">Activo: {{$empleado->activo == 1 ? 'Si': 'No'}}</label>
+                                        <label class="form-control">Telefono: {{$datoContacto->telefono}}</label>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-control">Direccion: {{$datoContacto->direccion}}</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-control">Estado: {{$datoContacto->estado}}</label>
                                     </div>
                                 </div>
                             </div>
@@ -46,7 +46,7 @@
                             <div class="row">
 
                                 <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <a href="{{ route('empleado.index') }}" class="btn btn-info btn-block" >Atrás</a>
+                                    <a href="{{ route('datoContacto.index') }}" class="btn btn-info btn-block" >Atrás</a>
                                 </div>
 
                             </div>
