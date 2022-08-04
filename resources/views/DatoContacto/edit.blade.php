e64de187b86fefddf86a2b5cf11ca4675db2770e
Hazibe <cjoselyn771@gmail.com>
2022-08-03 23:16:03 -0500

    EDIT FILE - resources/views/DatoContacto/edit.blade.php

--- resources/views/DatoContacto/edit.blade.php
+++ resources/views/DatoContacto/edit.blade.php
@@ -18,35 +18,22 @@
 
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title"> Editar Contacto</h3>
+                        <h3 class="panel-title"> Editar Datos de Contacto</h3>
                     </div>
                     <div class="panel-body">
-                        <form method="POST" action="{{route('datoContacto.update', $datoContactostat->id)}}" role="form">
+                        <form method="POST" action="{{route('datoContacto.update', $datoContacto->id)}}" role="form">
                             {{csrf_field()}}
                             <input name="_method" type="hidden" value="PUT">
 
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <input type="text" name="nombre" id="nombre" class="form-control input-sm" placeholder="Nombre del empleado" value="{{ old('nombre', $empleado->nombre) }}">
+                                        <input type="text" name="nombre" id="nombre" class="form-control input-sm" placeholder="Nombre del empleado" value="{{ old('nombre', $datoContacto->nombre) }}">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <input type="text" name="puesto" id="puesto" class="form-control input-sm" placeholder="Puesto del empleado" value="{{ old('puesto', $empleado->puesto) }}">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <input type="number" name="edad" id="edad" class="form-control input-sm" placeholder="Edad del empleado" value="{{ old('edad', $empleado->edad) }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <input type="number" name="salario" id="salario" class="form-control input-sm" placeholder="Salario del empleado" value="{{ old('salario', $empleado->salario) }}">
+                                        <input type="text" name="email" id="email" class="form-control input-sm" placeholder="Email del empleado" value="{{ old('email', $datoContacto->email) }}">
                                     </div>
                                 </div>
                             </div>
@@ -54,8 +41,19 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="form-control">Activo</label>
-                                        <input type="checkbox" name="activo" id="activo" class="form-control input-sm" {{$empleado->activo == 1 ? 'checked': ''}}>
+                                        <input type="number" name="telefono" id="telefono" class="form-control input-sm" placeholder="Telefono del empleado" value="{{ old('telefono', $datoContacto->telefono) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input type="text" name="direccion" id="direccion" class="form-control input-sm" placeholder="Direccion del empleado" value="{{ old('direccion', $datoContacto->direccion) }}">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                            <div class="col-md-4">
+                                    <div class="form-group">
+                                        <input type="text" name="estado" id="estado" class="form-control input-sm" placeholder="Estado" value="{{ old('estado', $datoContacto->estado) }}">
                                     </div>
                                 </div>
                             </div>
@@ -63,7 +61,7 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <button type="submit" class="btn btn-success" >Actualizar</button>
-                                    <a href="{{ route('empleado.index')  }}" class="btn btn-default"> Atras</a>
+                                    <a href="{{ route('datoContacto.index')  }}" class="btn btn-default"> Atras</a>
                                 </div>
                             </div>
                         </form>
